1d72fdf4958f840eb0899185aae633601282fddd
asifakhtar <shaikhasifakhtar@gmail.com>
2021-07-27 03:17:21 +0530

    navlink created

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import {FaBars} from 'react-icons/fa'
+import { Nav, NavContainer, NavLogo, MobileIcon, NavMenu , NavItem, NavLinks} from "./NavbarComponents";
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar</h2>
-        </div>
-    )
-}
+        <>
+            <Nav>
+                <NavContainer>
+                    <NavLogo to="/">ASIF</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About Me</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="about">My Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="about">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="about">Contact</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                </NavContainer>
+            </Nav>
+        </>
+    );
+};
 
-export default Navbar
+export default Navbar;
+
